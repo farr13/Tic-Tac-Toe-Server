d16b41281f65b006ca30f3751c8282abd5dc7db3
farr13 <abdoulaye.farrakhan@gmail.com>
2025-03-17 15:59:56 -0400

    tic tac toe game implmentation W.I.P

--- web_server.py
+++ web_server.py
@@ -1,22 +1,44 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+import socket
 
-def run_server():
-    host = '127.0.0.1'	# Localhost
-    port = 8080			# Port number
+class tic_tac_toe:
+    
+    def __init__(self):
+        self.player = 1
+        self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.moves = []
+    
+    def reset_game(self):
+        self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.moves = []
+    
+    def input_move(self, row, column):
+        if ([row, column] in self.moves):
+            print("Cannot make this move as it has already been made!")
+        else:
+            print(self.board[row][column])
+            self.board[row][column] = self.player
+            if(self.player == 1):
+                self.player = 2
+            else:
+                self.player = 1
+            self.moves.append([row, column])
+    
+    def display(self):
+        print(f"{self.board[0][0]} | {self.board[0][1]} | {self.board[0][2]}\n__________\n{self.board[1][0]} | {self.board[1][1]} | {self.board[1][2]}\n__________\n{self.board[2][2]} | {self.board[2][1]} | {self.board[0][2]}")
+    
 
-    # Create the server
-    server = HTTPServer((host, port), SimpleHTTPRequestHandler)
-    print(f"Server running at http://{host}:{port}/")
-    print("Press Ctrl+C to stop the server.")
+    def game_end(self, player):
+        for row in self.board:
+            pass
 
-   
+def main():
+    main_game = tic_tac_toe()
+    main_game.display()
 
- # Start the server
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print("\nServer stopped.")
-        server.server_close()
+    while(main_game.game_end()):
+        row = input("Input the row you want to play: ")
+        column = input("Input the column you want to play: ")
+        main_game.input_move(int(row), int(column))
+        main_game.display()
 
-if __name__ == "__main__":
-    run_server()
+main()
