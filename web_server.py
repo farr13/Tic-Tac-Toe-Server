71eea51d65985922387ffb4c48a4da0794584010
farr13 <abdoulaye.farrakhan@gmail.com>
2025-03-18 05:41:38 -0400

    Implemented tic-tac-toe

--- web_server.py
+++ web_server.py
@@ -6,6 +6,7 @@ class tic_tac_toe:
         self.player = 1
         self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         self.moves = []
+        self.gameEnd = False
     
     def reset_game(self):
         self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -15,30 +16,92 @@ class tic_tac_toe:
         if ([row, column] in self.moves):
             print("Cannot make this move as it has already been made!")
         else:
-            print(self.board[row][column])
             self.board[row][column] = self.player
-            if(self.player == 1):
-                self.player = 2
-            else:
-                self.player = 1
             self.moves.append([row, column])
+
+    def swap(self):
+        if(self.player == 1):
+            self.player = 2
+        else:
+            self.player = 1
     
     def display(self):
-        print(f"{self.board[0][0]} | {self.board[0][1]} | {self.board[0][2]}\n__________\n{self.board[1][0]} | {self.board[1][1]} | {self.board[1][2]}\n__________\n{self.board[2][2]} | {self.board[2][1]} | {self.board[0][2]}")
+        print(f"{self.board[0][0]} | {self.board[0][1]} | {self.board[0][2]}\n__________\n{self.board[1][0]} | {self.board[1][1]} | {self.board[1][2]}\n__________\n{self.board[2][0]} | {self.board[2][1]} | {self.board[2][2]}")
     
-
-    def game_end(self, player):
+    def game_end_tie(self):
         for row in self.board:
-            pass
+            for column in row:
+                if column == 0:
+                    return False
+        return True
 
+    def game_end(self):
+        #Vertical Win
+        if (self.board[0][0] == self.player & self.board[0][1] == self.player & self.board[0][2] == self.player):
+            print(f"Congrats player {self.player}, You Won!")
+            return True
+        elif (self.board[1][0] == self.player & self.board[1][1] == self.player & self.board[1][2] == self.player):
+            print(f"Congrats player {self.player}, You Won!")
+            return True
+        elif (self.board[2][0] == self.player & self.board[2][1] == self.player & self.board[2][2] == self.player):
+            print(f"Congrats player {self.player}, You Won!")
+            return True
+        #Horizontal Win
+        elif (self.board[0][0] == self.player & self.board[1][0] == self.player & self.board[2][0] == self.player):
+            print(f"Congrats player {self.player}, You Won!")
+            return True
+        elif (self.board[0][1] == self.player & self.board[1][1] == self.player & self.board[2][1] == self.player):
+            print(f"Congrats player {self.player}, You Won!")
+            return True
+        elif (self.board[0][2] == self.player & self.board[1][2] == self.player & self.board[2][2] == self.player):
+            print(f"Congrats player {self.player}, You Won!")
+            return True
+        #Diagonal Win
+        elif (self.board[0][0] == self.player & self.board[1][1] == self.player & self.board[2][2] == self.player):
+            print(f"Congrats player {self.player}, You Won!")
+            return True
+        elif (self.board[0][2] == self.player & self.board[1][1] == self.player & self.board[2][0] == self.player):
+            print(f"Congrats player {self.player}, You Won!")
+            return True
+        #Tie Game
+        elif self.game_end_tie():
+            print("Tie Game!")
+            return True
+        #No Win
+        else:
+            return False
+        
 def main():
     main_game = tic_tac_toe()
     main_game.display()
-
-    while(main_game.game_end()):
+    gameEnd = False
+    while(not gameEnd):
         row = input("Input the row you want to play: ")
         column = input("Input the column you want to play: ")
         main_game.input_move(int(row), int(column))
+        gameEnd = main_game.game_end()
+        main_game.swap()
         main_game.display()
 
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
